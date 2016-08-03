46338465e658acdbd86577045be633ffd7f1790f
Student P_02 <studentp_02@student-p-02.local>
2016-08-03 17:00:56 +0530

    Initial Commit

---
+++ ADCustom Cell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADCustom Cell
+//
+//  Created by Student P_02 on 03/08/16.
+//  Copyright © 2016 Amol Dhage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
